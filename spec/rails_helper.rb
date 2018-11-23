23465ee63a035fff8c783a5a2b1410a9b276faed
andrei <andyahont@gmail.com>
2018-11-23 19:04:32 +0600

    rcpec files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,6 +71,7 @@ RSpec.configure do |config|
   config.include RequestSpecHelper
   config.include ControllerSpecHelper
 
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
